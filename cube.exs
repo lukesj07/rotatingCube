350b6862d3b79453a17ad28c76820fe205a304ab
lukesj07 <lukesjennings07@gmail.com>
2024-07-20 21:20:29 -0400

    added rotating points

--- cube.exs
+++ cube.exs
@@ -1,6 +1,46 @@
 defmodule Cube do
   def main() do
+    vertices = [
+      [-10, 10, 10],
+      [-10, -10, 10],
+      [10, 10, 10],
+      [10, -10, 10],
+
+      [-10, 10, -10],
+      [-10, -10, -10],
+      [10, 10, -10],
+      [10, -10, -10]
+
+    ]
     
+
+    loop(vertices)
+  end
+
+  def loop(vertices) do
+    theta = 0.01
+    rot = Enum.map(vertices, fn vertex ->
+      [x, y, z] = vertex
+      p = []
+      p = [calculateZ(x, y, z, theta) | p]
+      p = [calculateY(x, y, z, theta) | p]
+      p = [calculateX(x, y, z, theta) | p]
+      p
+    end)
+
+    draw(rot)
+    loop(rot)
+  end
+
+  def draw(vertices) do
+    Enum.map(vertices, fn vertex ->
+      [x, y, _z] = Enum.map(vertex, fn coordinate -> Float.floor(coordinate) end)
+      # IO.puts(:io.rows()/2 - y, x + :io.columns()/2)
+      IO.write(IO.ANSI.cursor(round(elem(:io.rows(), 1)/2 - y), round(x + elem(:io.columns(), 1)/2)))
+      IO.write("$")
+    end)
+    :timer.sleep(10)
+    IO.write(IO.ANSI.clear())
   end
 
   def calculateX(x, y, z, theta) do
@@ -21,3 +61,5 @@ defmodule Cube do
     x * (sin * sin - cos * cos * sin) + y * (sin * sin * cos + sin * cos) + z * cos * cos
   end
 end
+
+Cube.main()
