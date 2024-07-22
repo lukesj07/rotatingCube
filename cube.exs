a0a204b2280766f92685ceee520a96d1a8411b52
CalebBunch <rcbcg123@gmail.com>
2024-07-22 18:21:04 -0400

    cleanups

--- cube.exs
+++ cube.exs
@@ -19,19 +19,19 @@ defmodule Cube do
   end
 
   def calculateIndices(vertices, indices, i1, i2) when i2 == length(vertices) do
-    calculateIndices(vertices, indices, i1+1, 0)
+    calculateIndices(vertices, indices, i1 + 1, 0)
   end
   
   def calculateIndices(vertices, indices, i1, i2) do
     if shared(Enum.at(vertices, i1), Enum.at(vertices, i2)) == 2 and [i2, i1] not in indices do
-      calculateIndices(vertices, indices ++ [[i1, i2]], i1, i2+1)
+      calculateIndices(vertices, indices ++ [[i1, i2]], i1, i2 + 1)
     else
-      calculateIndices(vertices, indices, i1, i2+1)
+      calculateIndices(vertices, indices, i1, i2 + 1)
     end
   end
   
   def shared(v1, v2) do
-    Enum.sum(for i <- 0..(length(v1)-1), do: (if Enum.at(v1, i) == Enum.at(v2, i), do: 1, else: 0))
+    Enum.sum(for i <- 0..(length(v1) - 1), do: (if Enum.at(v1, i) == Enum.at(v2, i), do: 1, else: 0))
   end
 
   def loop(vertices, connect) do
@@ -53,20 +53,20 @@ defmodule Cube do
       [v1, v2] = Enum.map(pair, fn i -> Enum.at(vertices, i) end)
       [x1, y1, _z1] = v1
       [x2, y2, _z2] = v2
-      yScale = 0.7
-      [screenY1, screenY2] = Enum.map([y1, y2], fn y -> round(elem(:io.rows(), 1)/2 - y * yScale) end)
-      [screenX1, screenX2] = Enum.map([x1, x2], fn x -> round(x + elem(:io.columns(), 1)/2) end)
+      yScale = 0.6
+      [screenY1, screenY2] = Enum.map([y1, y2], fn y -> round(elem(:io.rows(), 1) / 2 - y * yScale) end)
+      [screenX1, screenX2] = Enum.map([x1, x2], fn x -> round(x + elem(:io.columns(), 1) / 2) end)
       d = round(:math.sqrt(:math.pow((screenX2 - screenX1), 2) + :math.pow((screenY2 - screenY1), 2)))
       for s <- 0..d do
-        t = if d == 0, do: 0, else: s/d
+        t = if d == 0, do: 0, else: s / d
         x = round(screenX1 + t * (screenX2 - screenX1))
         y = round(screenY1 + t * (screenY2 - screenY1))
-        IO.write(IO.ANSI.cursor(y, x))
+        IO.ANSI.cursor(y, x) |> IO.write()
         IO.write("█")
       end
     end)
     :timer.sleep(25)
-    IO.write(IO.ANSI.clear())
+    IO.ANSI.clear() |> IO.write()
   end
 
   def calculateX(x, y, z, a, b, c) do
